8d51242d20c7f97d328bbdca1b07f7e0ae7ce0d0
fmezirard <floriane.mezirard@etudiant.univ-rennes2.fr>
2022-03-11 10:01:10 +0100

    Liste des astronautes ISS

--- astronautes.py
+++ astronautes.py
@@ -2,4 +2,7 @@ import requests
 
 contenu = requests.get("http://api.open-notify.org/astros.json")
 for personne in contenu.json()["people"]:
-    print(personne)
+    if personne["craft"] == "ISS":
+        print(personne)
+
+        
